82afbb22feb24d7d5b11b421496876ccc3dc810a
Emanuel <azazael567@gmail.com>
2025-07-10 19:31:54 -0600

    Add new routes to index

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+let urls = [];
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +24,42 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl',(req,res)=>{
+    const input = req.body.url;
+    if(!/^https?:\/\//.test(input)){
+        return res.json({err:'invalid url'});
+    }
+    let hostname;
+    try{
+      const parsedUrl = new URL (input);
+      hostname = parsedUrl.hostname;
+    }catch(err){
+      return res.json({err:'invalid url'})
+    }
+    
+    dns.lookup(hostname,(err,address)=>{
+        if(err){
+            return res.json({err:'invalid domain'})
+        }else{
+          const short = urls.length + 1;
+          urls.push({"original_url":input,"short_url":short});
+
+          res.json({"original_url":input,"short_url":short});
+        }
+    });
+});
+
+app.get('/api/shorturl/:short',(req,res)=>{
+    const short = parseInt(req.params.short);
+    const entry = urls.find(obj => obj.short_url == short);
+    if(entry){
+      res.redirect(entry.original_url);
+    }else{
+      res.status(404).send('URL no encontrada');
+    }
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
